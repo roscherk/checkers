d1023e72f09c002889775358f7bdbe0c4fdc1e4f
roscherk <70962699+roscherk@users.noreply.github.com>
2022-10-01 15:48:09 +0300

    first two rows of field using RelativePanel and Rectangle

--- GameWindow.axaml.cs
+++ GameWindow.axaml.cs
@@ -1,5 +1,7 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace checkers;
@@ -9,10 +11,35 @@ public partial class GameWindow : Window
     public GameWindow()
     {
         InitializeComponent();
+        AddHandler(DragDrop.DropEvent, Drop);
     }
 
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+        DragDrop.SetAllowDrop(this, true);
+    }
+
+    private void InputElement_OnPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void InputElement_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        var image = (Image)sender!;
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            var obj = new DataObject();
+            obj.Set("", image);
+            DragDrop.DoDragDrop(e, obj, DragDropEffects.Move);
+        }
+    }
+
+    private void Drop(object? sender, DragEventArgs e)
+    {
+        Console.WriteLine("Drop");
+        // Point dropPosition = e.GetPosition(Field);
+        // Console.WriteLine($"{dropPosition.X}, {dropPosition.Y}");
     }
 }
