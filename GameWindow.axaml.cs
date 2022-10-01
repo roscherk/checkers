e884095f685dc8ac7701e99e0bb3fcf63b85dbec
roscherk <70962699+roscherk@users.noreply.github.com>
2022-10-02 01:58:17 +0300

    drag and drop implemented, piece movement enhanced, pieces now can eat other pieces

--- GameWindow.axaml.cs
+++ GameWindow.axaml.cs
@@ -1,18 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using Avalonia;
-using Avalonia.Animation;
-using Avalonia.Animation.Animators;
+using System.Linq;
 using Avalonia.Controls;
-using Avalonia.Data;
 using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
-using Avalonia.Media.Transformation;
-using Avalonia.Styling;
 using Bitmap = Avalonia.Media.Imaging.Bitmap;
 using Image = Avalonia.Controls.Image;
 
@@ -21,6 +13,7 @@ namespace checkers;
 public partial class GameWindow : Window
 {
     private Grid _field = null!;
+    private Canvas _canvas = null!;
     private string _toMove = "W";  // W is for white, B is for black
     public GameWindow()
     {
@@ -30,6 +23,7 @@ public partial class GameWindow : Window
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+        _canvas = this.Find<Canvas>("CanvasDrag");
         _field = this.Find<Grid>("Field");
         _field.AddHandler(Avalonia.Input.DragDrop.DropEvent, DragDrop);
         _field.AddHandler(Avalonia.Input.DragDrop.DragOverEvent, DragOver);
@@ -90,41 +84,87 @@ public partial class GameWindow : Window
     {
         if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed) return;  // only left button moves the pieces
         var piece = (Image)sender!;
+        var draggable = new Image
+        {
+            Name = _toMove == "W" ? "WDrag" : "BDrag",
+            Source = new Bitmap(
+                $"/home/heapof/CLionProjects/checkers/Assets/sprite_{(_toMove == "W" ? "white" : "black")}.png"),
+            Height = 100,
+            Width = 100,
+            IsEnabled = false,
+            IsVisible = false
+        };
+        _canvas.Children.Add(draggable);
+        if (piece.Name!.StartsWith("W") && _toMove != "W"
+            || piece.Name.StartsWith("B") && _toMove != "B") return;  // player can move only pieces with appropriate color
         var obj = new DataObject();
-        var row = (int)e.GetPosition(_field).X / 100;
-        var column = (int)e.GetPosition(_field).Y / 100;
+        var row = (int)e.GetPosition(_field).Y / 100;
+        var column = (int)e.GetPosition(_field).X / 100;
         obj.Set("KeyValuePair<Image, KeyValuePair<int, int>>", new KeyValuePair<Image, KeyValuePair<int, int>>(piece, new KeyValuePair<int, int>(row, column)));
         Console.WriteLine($"\nDragStart: {piece.Name}");
-        var result = await Avalonia.Input.DragDrop.DoDragDrop(e, obj, DragDropEffects.Move);
+        await Avalonia.Input.DragDrop.DoDragDrop(e, obj, DragDropEffects.Move);
+    }
+    
+    private void DragOver(object? sender, DragEventArgs e)
+    {
+        var currentPosition = e.GetPosition(_canvas);
+        var (piece, (startRow, startColumn)) =
+            (KeyValuePair<Image, KeyValuePair<int, int>>)e.Data.Get("KeyValuePair<Image, KeyValuePair<int, int>>")!;
+        piece.IsVisible = false;
+        var draggable = (Image)_canvas.Children[1];
+        draggable.IsVisible = true;
+        Canvas.SetLeft(draggable, currentPosition.X - 50);
+        Canvas.SetTop(draggable, currentPosition.Y - 50);
     }
 
     private void DragDrop(object? sender, DragEventArgs e)
     {
-        try
-        {
-            var square = (Border)e.Source!;
-            Console.WriteLine($"Drop: {square.Name}");
+        _canvas.Children.RemoveRange(1, _canvas.Children.Count - 1);
+        Console.WriteLine("DragEnd ");
+        if (!e.Source!.ToString()!.EndsWith("Border")) return;
+        var square = (Border)e.Source!;
+        Console.WriteLine($"Drop: {square.Name}");
 
-            var row = square.Name![1] - '0';
-            var column = square.Name[2] - '0';
-            
-            if ((row + column) % 2 == 0) return;  // can't go on white squares  
+        var (piece, (startRow, startColumn)) =
+            (KeyValuePair<Image, KeyValuePair<int, int>>)e.Data.Get("KeyValuePair<Image, KeyValuePair<int, int>>")!;
+        var finishRow = square.Name![1] - '0';
+        var finishColumn = square.Name[2] - '0';
 
-            var data = (KeyValuePair<Image, KeyValuePair<int, int>>)e.Data.Get("KeyValuePair<Image, KeyValuePair<int, int>>")!;
-            var piece = data.Key;
-            _field.Children.Remove(piece);
-            Grid.SetRow(piece, row);
-            Grid.SetColumn(piece, column);
-            _field.Children.Add(piece);
-        }
-        catch (InvalidCastException exception)
+        // checking if the move is legal
+        Console.WriteLine($"startRow = {startRow}, finishRow = {finishRow}");
+        Console.WriteLine($"startColumn = {startColumn}, finishColumn = {finishColumn}");
+        if ((finishRow + finishColumn) % 2 == 0
+            || (_toMove == "W" && (finishRow > startRow || finishRow < startRow - 2))
+            || (_toMove == "B" && (finishRow < startRow || finishRow > startRow + 2))) return;
+        Image? toCapture = null; // checking whether we can capture somebody on this move
+        if (finishColumn == startColumn - 2)
         {
-            Console.WriteLine("Dropped onto another piece");
+            toCapture = _toMove == "W"
+                ? GetPiece(finishRow + 1, finishColumn + 1)
+                : GetPiece(finishRow - 1, finishColumn + 1);
         }
+        else if (finishColumn == startColumn + 2)
+        {
+            toCapture = _toMove == "W"
+                ? GetPiece(finishRow + 1, finishColumn - 1)
+                : GetPiece(finishRow - 1, finishColumn - 1);
+        }
+
+        if (toCapture != null)
+        {
+            _field.Children.Remove(toCapture);
+        }
+
+        _field.Children.Remove(piece);
+        Grid.SetRow(piece, finishRow);
+        Grid.SetColumn(piece, finishColumn);
+        piece.IsVisible = true;
+        _field.Children.Add(piece);
+        _toMove = _toMove == "W" ? "B" : "W";
+
     }
-
-    private void DragOver(object? sender, DragEventArgs e)
+    private Image? GetPiece(int row, int column)
     {
-        
+        return (from element in _field.Children where !element.ToString()!.EndsWith("Border") select (Image)element).FirstOrDefault(piece => Grid.GetRow(piece) == row && Grid.GetColumn(piece) == column);
     }
 }
