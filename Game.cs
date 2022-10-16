164f55dfe89470026836a9445805581435b408a6
roscherk <70962699+roscherk@users.noreply.github.com>
2022-10-16 22:35:26 +0300

    class Game created

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace checkers;
 
@@ -10,33 +11,36 @@ public class Game
         Black
     }
 
-    public struct Cell
+    public class Cell
     {
         public int X { get; private set; }
         public int Y { get; private set; }
         public Color CellColor { get; private set; }
+        public Piece? LinkedPiece { get; set; }
 
-        public Cell(int x, int y, Color cellColor)
+        public Cell(int x, int y)
         {
             X = x;
             Y = y;
-            CellColor = cellColor;
+            CellColor = (x + y) % 2 == 0 ? Color.White : Color.Black;
+            LinkedPiece = null;
         }
     }
-    
-    public struct Piece
-    {
-        public Cell OccupiedCell { get; set; }
-        public Color PieceColor { get; private set; }
-        public bool King { get; private set; }
 
-        public Piece(Cell occupiedCell, Color pieceColor, bool king=false)
+    public class Piece
+    {
+        public Piece(Cell occupiedCell, Color pieceColor, bool king = false)
         {
-            OccupiedCell = Field[occupiedCell.X][occupiedCell.Y];
+            OccupiedCell = occupiedCell;
             PieceColor = pieceColor;
             King = king;
         }
+
+        public Cell OccupiedCell { get; set; }
+        public Color PieceColor { get; private set; }
+        public bool King { get; set; }
     }
+
     public enum GameStatus
     {
         WhiteMove,
@@ -46,11 +50,12 @@ public class Game
         Draw
     }
 
-    private static readonly List<List<Cell>> Field = new List<List<Cell>>();
+    public Dictionary<Piece, List<Cell>> LegalMoves = new Dictionary<Piece, List<Cell>>();
+    private readonly List<List<Cell>> _field = new List<List<Cell>>();
     private readonly List<Piece> _pieces = new List<Piece>();
-    private Dictionary<Piece, List<Pair<int, int>>> _legalMoves = new Dictionary<Piece, List<Pair<int, int>>>();
     private readonly int _height;
     private readonly int _width;
+    private GameStatus _gameStatus = GameStatus.WhiteMove;
 
     public Game(int height, int width)
     {
@@ -58,27 +63,117 @@ public class Game
         _width = width;
         for (var i = 0; i < _height; ++i)
         {
-            Field.Add(new List<Cell>(_width));
+            _field.Add(new List<Cell>(_width));
             for (var j = 0; j < _width; ++j)
             {
-                Field[i].Add(new Cell(i, j, (i + j) % 2 == 0 ? Color.White : Color.Black));
+                _field[i].Add(new Cell(i, j));
+                if (_field[i][j].CellColor != Color.Black) continue;
                 switch (i)
                 {
-                    case <= 2 when Field[i][j].CellColor == Color.Black:
-                        _pieces.Add(new Piece(Field[i][j], Color.Black));
+                    case <= 2:
+                        _pieces.Add(new Piece(_field[i][j], Color.Black));
                         break;
-                    case >= 5 when Field[i][j].CellColor == Color.Black:
-                        _pieces.Add(new Piece(Field[i][j], Color.White));
+                    case >= 5:
+                        _pieces.Add(new Piece(_field[i][j], Color.White));
                         break;
                 }
+                _field[i][j].LinkedPiece = _pieces[^1];
             }
         }
     }
 
+    public List<Piece> GetPieces()
+    {
+        return _pieces.ToList();
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        return _field[x][y];
+    }
+
+    public GameStatus GetStatus()
+    {
+        return _gameStatus;
+    }
+    public void PassTheMove()
+    {
+        //TODO: проверить, не закончилась ли игра
+        _gameStatus = _gameStatus == GameStatus.WhiteMove ? GameStatus.BlackMove : GameStatus.WhiteMove;
+    }
+
+    private List<Cell> GetAvailableMoves(Piece piece)
+    {
+        var result = new List<Cell>();
+        var x = piece.OccupiedCell.X;
+        var y = piece.OccupiedCell.Y;
+        var k = 1;
+        
+        foreach (var i in new List<int> { -1, 1 })
+        {
+            if (!piece.King && piece.PieceColor == Color.Black)
+            {
+                continue;
+            }
+            
+            foreach (var j in new List<int> { -1, 1 })
+            {
+                while (x + k * i >= 0 && x + k * i < _height && y + k * j >= 0 && y + k * j < _width)
+                {
+                    if (_field[x + k*i][y + k*j].LinkedPiece == null)
+                    {
+                        result.Add(_field[x + k*i][y + k*j]);
+                    } else if (x + (k + 1)*i >= 0 && x + (k + 1)*i < _height &&
+                               y + (k + 1)*j >= 0 && y + (k + 1)*j < _width)
+                    {
+                        result.Add(_field[x + (k + 1)*i][y + (k + 1)*j]);
+                    }
+                    k++;
+                    if (!piece.King)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (!piece.King)
+            {
+                break;
+            }
+        }
+        // if (piece.King)
+        // {
+        //     
+        // }
+        // else
+        // {
+        //     foreach (var j in new List<int> { -1, 1 })
+        //     {
+        //         if ((y == 0 && j == -1) || (y == _width - 1 && j == 1))
+        //         {
+        //             // проверяем, что не вышли за границы поля
+        //             continue;
+        //         }
+        //
+        //         var i = piece.PieceColor == Color.White ? -1 : 1;
+        //         if (_field[x + i][y + j].LinkedPiece == null)
+        //         {
+        //             result.Add(_field[x + i][y + j]);
+        //         } else if (x + 2*i >= 0 && x + 2*i < _height && y + 2*j >= 0 && y + 2*j < _width)
+        //         {
+        //             result.Add(_field[x + 2*i][y + 2*j]);
+        //         }
+        //     }
+        // }
+        return result;
+    }
+
     public void UpdateMoves()
     {
-        return;
+        foreach (var piece in _pieces.Where(piece => (int)piece.PieceColor == (int)_gameStatus))
+        {
+            LegalMoves.Add(piece, GetAvailableMoves(piece));
+        }
     }
-    
+
     // private List<Pair<int, int>> 
 }
--- GameWindow.axaml.cs
+++ GameWindow.axaml.cs
@@ -1,46 +1,231 @@
 using System;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
+using Image = Avalonia.Controls.Image;
 
 namespace checkers;
 
+
+
 public partial class GameWindow : Window
 {
-    private RelativePanel _field = null!;
+    private Grid _fieldImage = null!;
+    private Canvas _canvas = null!;
+    private readonly Game _game;
+    private const int FieldHeight = 8;
+    private const int FieldWidth = 8;
+
     public GameWindow()
     {
+        _game = new Game(FieldHeight, FieldWidth);  //TODO: добавить возможность настраивать размеры поля
         InitializeComponent();
-        AddHandler(DragDrop.DropEvent, Drop);
     }
 
     private void InitializeComponent()
     {
+        _canvas = this.Find<Canvas>("CanvasDrag");
+        _fieldImage = this.Find<Grid>("Field");
+        _fieldImage.AddHandler(Avalonia.Input.DragDrop.DropEvent, DragDrop);
+        _fieldImage.AddHandler(Avalonia.Input.DragDrop.DragOverEvent, DragOver);
+
+        // render the field image
+        for (var i = 0; i < FieldHeight; ++i)
+        {
+            for (var j = 0; j < FieldWidth; ++j)
+            {
+                var border = new Border
+                {
+                    Name = $"S{i}{j}",
+                    Background = (i + j) % 2 == 0 ? SolidColorBrush.Parse("White") : SolidColorBrush.Parse("Brown")
+                };
+                Grid.SetRow(border, i);
+                Grid.SetColumn(border, j);
+                _fieldImage.Children.Add(border);
+            }
+        }
+
+        // fill the field with the pieces
+        foreach (var piece in _game.GetPieces())
+        {
+            var pieceImage = new Image
+            {
+                Source = new Bitmap(
+                    $"/home/heapof/CLionProjects/checkers/Assets/sprite_{(piece.PieceColor == Game.Color.White ? "white" : "black")}.png"),
+            };
+            pieceImage.PointerPressed += DragStart;
+            Grid.SetRow(pieceImage, piece.OccupiedCell.X);
+            Grid.SetColumn(pieceImage, piece.OccupiedCell.Y);
+            _fieldImage.Children.Add(pieceImage);
+        }
+
+        Avalonia.Input.DragDrop.SetAllowDrop(_fieldImage, true);
         AvaloniaXamlLoader.Load(this);
-        _field = this.Find<RelativePanel>("Field");
-        DragDrop.SetAllowDrop(_field, true);
-        Console.WriteLine(_field.Bounds.Size);
     }
 
-    private void InputElement_OnPointerPressed(object sender, PointerPressedEventArgs e)
+    private async void DragStart(object? sender, PointerPressedEventArgs e)
     {
-        throw new NotImplementedException();
-    }
+        var gameStatus = _game.GetStatus();
+        if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed ||
+            (gameStatus != Game.GameStatus.BlackMove &&
+             gameStatus != Game.GameStatus.WhiteMove)) return;
+        var piece = (Image)sender!;
+        if (piece.Name!.StartsWith("W") && gameStatus != Game.GameStatus.WhiteMove
+            || piece.Name.StartsWith("B") && gameStatus != Game.GameStatus.BlackMove) return;  // player can move their pieces only
 
-    private void InputElement_OnPointerMoved(object? sender, PointerEventArgs e)
-    {
-        var image = (Image)sender!;
-        if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed) return;
+        _game.UpdateMoves();
+        
+        var row = (int)e.GetPosition(_fieldImage).Y / 100;
+        var column = (int)e.GetPosition(_fieldImage).X / 100;
+        var linkedPiece = _game.GetCell(row, column).LinkedPiece ?? new Game.Piece(new Game.Cell(0, 0), Game.Color.Black);
+        if (!_game.LegalMoves.ContainsKey(linkedPiece))
+        {
+            //TODO: добавить анимацию, показывающую невозможность хода
+            return;
+        }
+        
+        // start dragging
+        var draggable = new Image
+        {
+            Name = gameStatus == Game.GameStatus.WhiteMove ? "Wdrag" : "Bdrag",
+            Source = new Bitmap($"/home/heapof/CLionProjects/checkers/Assets/sprite_{(gameStatus == Game.GameStatus.WhiteMove ? "white" : "black")}.png"),
+            Height = 100,
+            Width = 100,
+            IsEnabled = false,
+            IsVisible = false
+        };
+        _canvas.Children.Add(draggable);
         var obj = new DataObject();
-        obj.Set("", image);
-        DragDrop.DoDragDrop(e, obj, DragDropEffects.Move);
+        obj.Set("Pair<Image image, Game.Piece piece>", new Pair<Image, Game.Piece>(piece, linkedPiece));
+        Console.WriteLine($"\nDragStart: {piece.Name}");
+        await Avalonia.Input.DragDrop.DoDragDrop(e, obj, DragDropEffects.Move);
     }
 
-    private void Drop(object? sender, DragEventArgs e)
+    private void DragOver(object? sender, DragEventArgs e)
     {
-        Console.WriteLine("Drop");
-        // Point dropPosition = e.GetPosition(Field);
-        // Console.WriteLine($"{dropPosition.X}, {dropPosition.Y}");
+        var currentPosition = e.GetPosition(_canvas);
+        var (piece, _) = (Pair<Image, Game.Piece>)e.Data.Get("Pair<Image image, Game.Piece piece>")!;
+        piece.IsVisible = false;
+        var draggable = (Image)_canvas.Children[1];
+        draggable.IsVisible = true;
+        Canvas.SetLeft(draggable, currentPosition.X - 50);
+        Canvas.SetTop(draggable, currentPosition.Y - 50);
     }
+
+    private void DragDrop(object? sender, DragEventArgs e)
+    {
+        _canvas.Children.RemoveRange(1, _canvas.Children.Count - 1); // remove draggable
+        var (piece, linkedPiece) =
+            (Pair<Image, Game.Piece>)e.Data.Get("Pair<Image image, Game.Piece piece>")!;
+        var finishRow = (int)e.GetPosition(_fieldImage).Y / 100;
+        var finishColumn = (int)e.GetPosition(_fieldImage).X / 100;
+        piece.IsVisible = true;
+        // checking if the move is legal
+        if (!e.Source!.ToString()!.EndsWith("Border")
+            || _game.LegalMoves[linkedPiece].Contains(new Game.Cell(finishRow, finishColumn))) return;
+        // Console.WriteLine($"startRow = {startRow}, finishRow = {finishRow}");
+        // Console.WriteLine($"startColumn = {startColumn}, finishColumn = {finishColumn}");
+        
+        // handle capturing
+        // Image? toCapture = null;
+        // if (finishColumn == startColumn - 2)
+        // {
+        //     toCapture = _toMove == "W" ? GetPiece(finishRow + 1, finishColumn + 1)
+        //         : GetPiece(finishRow - 1, finishColumn + 1);
+        // }
+        // else if (finishColumn == startColumn + 2)
+        // {
+        //     toCapture = _toMove == "W" ? GetPiece(finishRow + 1, finishColumn - 1)
+        //         : GetPiece(finishRow - 1, finishColumn - 1);
+        // }
+        //
+        // if (toCapture != null)
+        // {
+        //     _fieldImage.Children.Remove(toCapture);
+        // }
+        
+        // change the piece position
+        _fieldImage.Children.Remove(piece);
+        Grid.SetRow(piece, finishRow);
+        Grid.SetColumn(piece, finishColumn);
+        _fieldImage.Children.Add(piece);
+        
+        // if no follow-up is available, pass the move
+        // if there is a follow-up, continue the move
+        // generate possible moves
+        Console.WriteLine($"DragEnd, droped: Row {finishRow} Column {finishColumn}");
+    }
+
+    // private void UpdateMoves(Pair<int, int>? followUp=null)
+    // {
+    //     _possibleMoves.Clear();
+    //     if (followUp != null)
+    //     {
+    //         return;
+    //     }
+    //
+    //     for (var i = 1; i < 9; ++i)
+    //     {
+    //         for (var j = 1; j < 9; ++j)
+    //         {
+    //             if (_field[i][j] != _toMove)
+    //             {
+    //                 continue;
+    //             }
+    //
+    //             var coordinates = new Pair<int, int>(i, j);
+    //             _possibleMoves.Add(coordinates, new List<Pair<int, int>>());
+    //             if (_field[i - 1][j - 1] != _toMove)
+    //             {
+    //                 _possibleMoves[coordinates].Add(new Pair<int, int>(i - 2, j - 2));
+    //             }
+    //             if (_field[i - 1][j + 1] != _toMove)
+    //             {
+    //                 _possibleMoves[coordinates].Add(new Pair<int, int>(i - 2, j + 2));
+    //             }
+    //             if (_field[i + 1][j - 1] != _toMove)
+    //             {
+    //                 _possibleMoves[coordinates].Add(new Pair<int, int>(i + 2, j - 2));
+    //             }
+    //             if (_field[i + 1][j + 1] != _toMove)
+    //             {
+    //                 _possibleMoves[coordinates].Add(new Pair<int, int>(i + 2, j + 2));
+    //             }
+    //             
+    //             foreach (var move in new List<Pair<int, int>>(_possibleMoves[coordinates]))
+    //             {
+    //                 if (move.First is < 1 or > 8 || move.Second is < 1 or > 8)
+    //                 {
+    //                     _possibleMoves[coordinates].Remove(move);
+    //                 }
+    //             }
+    //
+    //             if (_possibleMoves[coordinates].Count == 0)
+    //             {
+    //                 if (_toMove == "W")
+    //                 {
+    //                     _possibleMoves[coordinates].Add(new Pair<int, int>(i - 1, j - 1));
+    //                     _possibleMoves[coordinates].Add(new Pair<int, int>(i - 1, j + 1));
+    //                 }
+    //                 else
+    //                 {
+    //                     _possibleMoves[coordinates].Add(new Pair<int, int>(i + 1, j - 1));
+    //                     _possibleMoves[coordinates].Add(new Pair<int, int>(i + 1, j + 1));
+    //                 }
+    //             }
+    //
+    //             foreach (var move in new List<Pair<int, int>>(_possibleMoves[coordinates]))
+    //             {
+    //                 if (move.First is < 1 or > 8 || move.Second is < 1 or > 8)
+    //                 {
+    //                     _possibleMoves[coordinates].Remove(move);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
